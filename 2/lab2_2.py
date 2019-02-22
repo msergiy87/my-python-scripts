502d43530337db788279ea95b2fb90bfdca63111
msergiy87 <msergiy87@protonmail.com>
2019-02-22 20:31:39 +0200

    some changes to lab2

--- 2/lab2_2.py
+++ 2/lab2_2.py
@@ -1,29 +1,33 @@
-# print sound text. Insert 3 numbers as a parametr
+# print sound text. Insert 3 arduments as a numbers
 
 import sys
 
-x = int(sys.argv[1])
-y = int(sys.argv[2])
-z = int(sys.argv[3])
+x = int(sys.argv[1])	# kilkist la-
+y = int(sys.argv[2])	# kilkist kypletiv
+z = int(sys.argv[3])	# end symbol
 
-# My solution
-string = "Everybody sing a song: "
+song_string = "Everybody sing a song:"
+
+if x <= 0:
+  print "x shouldnt be 0 and positive"
+  quit()
 
 if z == 1:
-    end = "!"
+  song_end = "!"
+elif z == 0:
+  song_end = "."
 else:
-    end = "."
+  print "z should be 0 or 1"
+  quit()
 
-if y == 0:
-    string = string[:-1]
+if y < 0:
+  print "y should be positive"
+  quit()
 
-la = "la-" * x
-la = la[:-1]
+kyplet = (' ' + 'la-'*(x-1) + 'la')*y
 
-kypl = (la+" ") * y
-kypl = kypl[:-1]
-
-print string + kypl + end
+print song_string + kyplet + song_end
 
 # Teacher solution
 print 'Everybody sing a song:' + (' ' + 'la-'*(x-1) + 'la')*y + '!'*z + '.'*(1-z)
+ 
